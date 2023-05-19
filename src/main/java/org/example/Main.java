ac79672669d0070b162bc3b064ff1a05441e799e
luizfellipes <luiz.13.silva@gmail.com>
2023-05-19 11:13:56 -0400

    Adicionado pessoaFisica e Juridica, adicionado os juros no metodo ValorTotalPago

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
